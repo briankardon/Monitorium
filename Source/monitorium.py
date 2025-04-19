340516b3ebdeabebac8a5ecc5f2e5f6759e3c884
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2025-04-19 17:11:42 -0400

    Added working config file to service

--- Source/monitorium.py
+++ Source/monitorium.py
@@ -6,6 +6,7 @@ import socket
 import sys
 import time
 from pathlib import Path
+import yaml
 
 # Thanks for the guidance from:
 #   https://dev.to/demola12/building-a-robust-windows-service-in-python-with-win32serviceutil-part-13-1k6k
@@ -21,6 +22,7 @@ class MonitoriumService(win32serviceutil.ServiceFramework):
         self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
         socket.setdefaulttimeout(120)
         self.is_alive = True
+        self.monitorium_params = None
 
     def SvcStop(self):
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
@@ -33,11 +35,54 @@ class MonitoriumService(win32serviceutil.ServiceFramework):
                               (self._svc_name_, ''))
         self.main()
 
+    def load_config(self):
+        # Load config file
+        if getattr(sys, 'frozen', False):
+            # If the application is run as a bundle, the PyInstaller bootloader
+            # extends the sys module by a flag frozen=True and sets the app
+            # path into variable _MEIPASS'.
+            application_path = Path(sys._MEIPASS).resolve()
+        else:
+            application_path = Path.absolute(__file__).resolve().parent
+
+        config_path = application_path / 'config.yml'
+
+        success = True
+
+        try:
+            servicemanager.LogErrorMsg("Looking for config file: {f}".format(f=str(config_path)))
+            with open(config_path, 'r') as f:
+                self.monitorium_params = yaml.safe_load(f)
+        except FileNotFoundError:
+            servicemanager.LogErrorMsg("Couldn't find config file!")
+            success = False
+        except yaml.YAMLError as exc:
+            if hasattr(exc, 'problem'):
+                error_msg = exc.problem
+            else:
+                error_msg = 'unknown problem'
+            servicemanager.LogErrorMsg("Error parsing config file: {problem}".format(problem=error_msg))
+            success = False
+
+        # Check that required config params exist:
+        required_params = [
+            'newname',
+        ]
+        for required_param in required_params:
+            if required_param not in self.monitorium_params:
+                servicemanager.LogErrorMsg("Error in config file - missing required parameter \"{param}\"".format(param=required_param))
+                success = False
+
+        if not success:
+            raise Exception('Configuration error!')
+
     def main(self):
         # Main service logic goes here
+        self.load_config()
+
         root = Path(r'C:\Users\briankardon\Downloads')
         name = 'test.txt'
-        newname = 'test_CHANGED.txt'
+        newname = self.monitorium_params['newname']
         path = root / name
         newpath = root / newname
         while self.is_alive:
